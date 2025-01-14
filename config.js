2469c713c023664c06d6ff2389bd19b202d9116d
Juancams <jc.manzanares.serrano@gmail.com>
2025-01-14 23:55:24 +0100

    Update
    Signed-off-by: Juancams <jc.manzanares.serrano@gmail.com>

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'Juan Carlos Manzanares Serrano', // <title>
+  siteUrl: 'https://Juancams.github.io/',
+  manifestName: 'Juancams',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: ``, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Juan Carlos Manzanares Serrano',
+  heading: 'Robotics researcher',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/Juancams',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/Juancams98',
+    },
+    {
+      icon: 'fa-linkedin',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/juancams/',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:jc.manzanares.serrano@gmail.com',
+    },
+  ],
+};
